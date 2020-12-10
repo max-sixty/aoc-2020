617c1f2e8e480ecb63c41b4cecfb2e812b0ddf73
Maximilian Roos <m@maxroos.com>
2020-12-10 00:38:59 -0800

    Replace groupcount with countmap

--- Day-10.jl
+++ Day-10.jl
@@ -1,18 +1,15 @@
-using Memoize, SplitApplyCombine, Pipe
+using Memoize, Pipe, StatsBase
 
 str = read(open("Day-10.txt"), String)
-
 ns = @pipe str |> strip |> split .|> parse(Int, _) |> sort |> [0, _..., maximum(_) + 3]
 
-part1 = @pipe ns |> diff |> groupcount(x -> x, _) |> _[1] * _[3]
-
+part1 = @pipe ns |> diff |> countmap |> _[1] * _[3]
 println(part1)
 
-@memoize function combinations(i)
+@memoize function n_combos(i)
     i == length(ns) && return 1
-    return sum(combinations(j) for j in i+1:length(ns) if ns[j] - ns[i] <= 3)
+    sum(n_combos(j) for j in i+1:length(ns) if ns[j] - ns[i] <= 3)
 end
 
-part2 = combinations(1)
-
+part2 = n_combos(1)
 println(part2)
