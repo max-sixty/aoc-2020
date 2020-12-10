9f0842276c279fb27b470b120b02056dd396f9ba
Maximilian Roos <m@maxroos.com>
2020-12-09 23:51:28 -0800

    Day 10 refinements

--- Day-10.jl
+++ Day-10.jl
@@ -1,29 +1,18 @@
-using Memoize
+using Memoize, SplitApplyCombine, Pipe
 
 str = read(open("Day-10.txt"), String)
 
-ns = (
-    str |>
-    strip |>
-    split .|>
-    (x -> parse(Int, x)) |>
-    sort |>
-    x -> [0, x..., maximum(x) + 3]
-)
+ns = @pipe str |> strip |> split .|> parse(Int, _) |> sort |> [0, _..., maximum(_) + 3]
 
-diffs = diff(ns)
+part1 = @pipe ns |> diff |> groupcount(x -> x, _) |> _[1] * _[3]
 
-function groupby(a)
-    out = Dict()
-    [out[n] = get!(out, n, 0) + 1 for n in a]
-    out
-end
-
-println(groupby(diffs) |> x -> x[3] * x[1])
+println(part1)
 
 @memoize function combinations(i)
     i == length(ns) && return 1
     return sum(combinations(j) for j in i+1:length(ns) if ns[j] - ns[i] <= 3)
 end
 
-println(combinations(1))
+part2 = combinations(1)
+
+println(part2)
