b2125b8c0b1bac04c6dcc660e2b12afdb863a80b
Maximilian Roos <m@maxroos.com>
2020-12-20 22:19:29 -0800

    Reorg Day 21 slightly

--- Day-21.jl
+++ Day-21.jl
@@ -13,14 +13,19 @@ all_foods = @_ foods .|> _.food |> flatten |> countmap
 all_allergens = @_ foods .|> _.allergen |> flatten |> countmap
 
 shortlists = map(all_allergens |> keys |> collect) do a
-    allergen_matching = [f for f in foods if a in f.allergen]
+    allergen_matching = @_ filter(a in _.allergen, foods)
     a => intersect([f.food for f in allergen_matching]...)
 end |> Dict
 
+potential_contain_allergens = shortlists |> values |> flatten |> unique
+cannot_contain = @_ all_foods |> keys |> setdiff(__, potential_contain_allergens)
+
+part_1 = sum(c for (f, c) in all_foods if f in cannot_contain)
+println(part_1)
+
 function to_food_allergen_map(shortlists)
     result = Dict()
-
-    shortlists = deepcopy(shortlists)
+    shortlists = copy(shortlists)
 
     while length(shortlists) > 0
         allergen = @_ filter(length(_[2]) == 1, shortlists) |> first |> __[1]
@@ -33,13 +38,8 @@ function to_food_allergen_map(shortlists)
 end
 
 mapping = to_food_allergen_map(shortlists)
-potential_contain_allergens = shortlists |> values |> flatten |> unique
-cannot_contain = @_ all_foods |> keys |> setdiff(__, potential_contain_allergens)
-
-part_1 = sum(c for (f, c) in all_foods if f in cannot_contain)
 part_2 = @_ mapping |> collect |> sort(__, by = _[1]) |> map(_[2], __) |> join(__, ",")
 
-println(part_1)
 println(part_2)
 
 
