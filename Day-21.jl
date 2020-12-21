ca97323f916f50416872d155cf6906b7308476a0
Maximilian Roos <m@maxroos.com>
2020-12-20 22:26:56 -0800

    Some renames in Day 21

--- Day-21.jl
+++ Day-21.jl
@@ -3,27 +3,27 @@ using Underscores, StatsBase, Base.Iterators
 input = read(open("Day-21.txt"), String) |> strip
 
 foods = map(split(input, "\n")) do l
-    foods_str, allergens_str = match(r"(.*) \(contains (.*)\)", l).captures
-    food = split(foods_str |> strip, " ")
+    ingredients_str, allergens_str = match(r"(.*) \(contains (.*)\)", l).captures
+    ingredient = split(ingredients_str |> strip, " ")
     allergen = split(allergens_str |> strip, ", ")
-    (; food, allergen)
+    (; ingredient, allergen)
 end
 
-all_foods = @_ foods .|> _.food |> flatten |> countmap
-all_allergens = @_ foods .|> _.allergen |> flatten |> countmap
+ingredients_count = @_ foods .|> _.ingredient |> flatten |> countmap
+allergens_count = @_ foods .|> _.allergen |> flatten |> countmap
 
-shortlists = map(all_allergens |> keys |> collect) do a
-    allergen_matching = @_ filter(a in _.allergen, foods)
-    a => intersect([f.food for f in allergen_matching]...)
+shortlists = map(allergens_count |> keys |> collect) do a
+    foods_matching = @_ filter(a in _.allergen, foods)
+    a => intersect([f.ingredient for f in foods_matching]...)
 end |> Dict
 
 potential_contain_allergens = shortlists |> values |> flatten |> unique
-cannot_contain = @_ all_foods |> keys |> setdiff(__, potential_contain_allergens)
+cannot_contain = @_ ingredients_count |> keys |> setdiff(__, potential_contain_allergens)
 
-part_1 = sum(c for (f, c) in all_foods if f in cannot_contain)
+part_1 = sum(c for (f, c) in ingredients_count if f in cannot_contain)
 println(part_1)
 
-function to_food_allergen_map(shortlists)
+function to_map(shortlists)
     result = Dict()
     shortlists = copy(shortlists)
 
@@ -37,9 +37,7 @@ function to_food_allergen_map(shortlists)
     result
 end
 
-mapping = to_food_allergen_map(shortlists)
+mapping = to_map(shortlists)
 part_2 = @_ mapping |> collect |> sort(__, by = _[1]) |> map(_[2], __) |> join(__, ",")
 
 println(part_2)
-
-
