dbac98e08eb931f7cf903c6eef1b262579061ff3
Maximilian Roos <m@maxroos.com>
2020-12-21 22:39:18 -0800

    Use the actual hash function

--- Day-22.jl
+++ Day-22.jl
@@ -9,7 +9,6 @@ decks = map(decks_str) do player
     @_ lines[2:end] .|> parse(Int, _)
 end
 
-hash_decks(decks) = [[decks[1]..., 0, decks[2]...]]
 copy_cards(deck) = copy(deck[2:(deck[1]+1)])
 is_too_short(deck) = deck[1] > length(deck) - 1
 
@@ -20,11 +19,9 @@ function play_recurse_game(decks, history)
 
     while all(length.(decks) .> 0)
 
-        if hash_decks(decks) in history
-            return 1, decks
-        end
+        hash(decks) ∈ history && return 1, decks
 
-        push!(history, hash_decks(decks))
+        push!(history, hash(decks))
 
         if any(is_too_short.(decks))
             winner = decks[1][1] > decks[2][1] ? 1 : 2
@@ -32,13 +29,13 @@ function play_recurse_game(decks, history)
             winner, _ = play_recurse_game(copy_cards.(decks), Set())
         end
 
+        c1, c2 = popfirst!.(decks)
         if winner == 1
-            push!(decks[1], decks[1][1], decks[2][1])
+            push!(decks[1], c1, c2)
         else
-            push!(decks[2], decks[2][1], decks[1][1])
+            push!(decks[2], c2, c1)
         end
 
-        deleteat!.(decks, 1)
     end
     winner = length(decks[1]) > length(decks[2]) ? 1 : 2
     winner, decks
